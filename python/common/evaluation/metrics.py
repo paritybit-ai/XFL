fix(xgboost): fix incorrect result for categorial feataure in some cases

python/common/evaluation/metrics.py
@@ -118,7 +118,9 @@ class ThresholdCutter:
     def __init__(self, output_file=None):
         self.bst_threshold = 0.5
         self.bst_score = 0
-        self.default_percentile = np.arange(95, 0.0, -5)
+        # self.default_threshold = [0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.46, 0.47, 0.48, 0.49, 0.5,
+        #                           0.51, 0.52, 0.53, 0.54, 0.55, 0.6, 0.65, 0.7, 0.8, 0.9]
+        self.default_percentile = np.arange(100, -1, -1)
         self.output_file = output_file
         self.metrics = {
             "threshold": [],
@@ -133,16 +135,16 @@ class ThresholdCutter:
 
     def cut_by_value(self, y_true: np.array, y_pred: np.array, values: List = None):
         probs = np.unique(y_pred)
-        logger.info("num of probs: %d" % len(probs))
+        logger.info("num of probs: %d." % len(probs))
         if values is None:
             if len(probs) < len(self.default_percentile):
-                logger.warning(
-                    "ks points %d less than the default num: %d" % (len(probs), len(self.default_percentile)))
+                # logger.warning("ks points %d less than the default num: %d." % (len(probs),
+                #                                                                 len(self.default_percentile)))
                 values = np.array(sorted(probs, reverse=True))
                 self.default_percentile = np.array([sum(y_pred < _) / (len(y_pred) - 1) * 100 for _ in values])
             else:
                 values = np.percentile(y_pred, self.default_percentile)
-        # - Threshold, TP, FN, FP, TN, TPR, FPR, KS
+        # -	Threshold, TP, FN, FP, TN, TPR, FPR, KS
         for threshold in values:
             tn, fp, fn, tp = confusion_matrix(y_true, y_pred >= threshold).ravel()
             if tp + fn > 0:
@@ -153,7 +155,7 @@ class ThresholdCutter:
                 fpr = fp / (tn + fp)
             else:
                 fpr = np.nan
-            ks = tpr - fpr
+            ks = max(np.max(tpr - fpr), 0)
             for metric in self.metrics:
                 self.metrics[metric].append(locals()[metric])
             if ks > self.bst_score:
@@ -179,8 +181,16 @@ class ThresholdCutter:
 class DecisionTable:
     def __init__(self, conf):
         self.method = conf.get("method", "equal_frequency")
-        self.bin_number = conf.get("bin_number", 10)
+        self.bin_number = conf.get("bin_number", 5)
+        self.type = conf.get("type")
+        self._check_params()
         self.stats = pd.DataFrame()
+
+    def _check_params(self):
+        if self.method not in ("equal_frequency", "equal_width"):
+            raise NotImplementedError("decision table: method '{}' is not implemented.".format(self.method))
+        if self.bin_number <= 1:
+            raise ValueError("decision table: bin number ({}) must be greater than 1.".format(self.bin_number))
 
     def fit(self, y_true: np.array, y_pred: np.array):
         df = pd.DataFrame({"label": y_true, "pred": y_pred})
@@ -188,8 +198,8 @@ class DecisionTable:
             groups = pd.qcut(y_pred, self.bin_number, duplicates='drop', precision=3)
         elif self.method == "equal_width":
             groups = pd.cut(y_pred, self.bin_number, right=True, duplicates='drop', precision=3)
-        else:
-            raise NotImplementedError("decision table's method {} is not implemented.".format(self.method))
+        if self.type == "score_card":
+            groups = [pd.Interval(int(_.left), int(_.right), _.closed) for _ in groups]
         df["区间"] = groups
         self.stats["样本数"] = df.groupby("区间").size()
         self.stats["负样本数"] = df.groupby("区间")["label"].agg(lambda x: sum(x == 0))
@@ -220,6 +230,7 @@ class DecisionTable:
         self.stats["区间"] = self.stats["区间"].apply(str)
 
     def save(self, file_name):
+        logger.info("decision table: saved to {}.".format(file_name))
         self.stats.to_csv(file_name, header=True, index=False, float_format='%.2g')
 
 

test/algorithm/common/evaluation/test_metrics.py
@@ -33,52 +33,87 @@ def env():
 
 
 class TestDecisionTable:
-	@pytest.mark.parametrize('bins', [1, 2, 5, 10, 50])
-	def test_fit(self, bins, env):
-		file_path = "/opt/checkpoints/decision_table.csv"
-		dt = DecisionTable({"bin_number": bins})
-		y_true = np.array([1] * 50000 + [0] * 50000)
-		np.random.shuffle(y_true)
-		y_pred = np.random.normal(0, 1, size=100000)
-		dt.fit(y_true, y_pred)
-		assert len(dt.stats) == bins
-		dt.save(file_path)
-		# read_from_file
-		df = pd.read_csv(file_path)
-		assert len(df) == bins
-		assert df.iloc[-1]["累计总样本数"] == 100000
-		assert df.iloc[-1]["累计负样本数"] == 50000
-		assert df.iloc[-1]["累计负样本/负样本总数"] == "100.00%"
-		assert df.iloc[-1]["累计正样本/正样本总数"] == "100.00%"
-		assert df.iloc[-1]["累计负样本/累计总样本"] == "50.00%"
+	@pytest.mark.parametrize('bins', [-1, 0, 1, 2, 5, 10, 50])
+	def test_bins(self, bins, env):
+		io_file_path = "/opt/checkpoints/decision_table_{}.csv".format(bins)
+		config = {"bin_number": bins}
+
+		if bins <= 1:
+			with pytest.raises(ValueError) as e:
+				dt = DecisionTable(config)
+		else:
+			dt = DecisionTable(config)
+			# prepare a random test data
+			y_true = np.array([1] * 50000 + [0] * 50000)
+			y_pred = np.random.normal(0, 1, size=100000)
+			np.random.shuffle(y_true)
+
+			dt.fit(y_true, y_pred)
+			assert len(dt.stats) == bins
+			dt.save(io_file_path)
+			# read_from_file
+			df = pd.read_csv(io_file_path)
+			assert len(df) == bins
+			assert (df["样本数"] == (100000 / bins)).all()
+			assert df.iloc[-1]["累计总样本数"] == 100000
+			assert df.iloc[-1]["累计负样本数"] == 50000
+			assert df.iloc[-1]["累计负样本/负样本总数"] == "100.00%"
+			assert df.iloc[-1]["累计正样本/正样本总数"] == "100.00%"
+			assert df.iloc[-1]["累计负样本/累计总样本"] == "50.00%"
 
 	@pytest.mark.parametrize("method", ["equal_frequency", "equal_width", 'other'])
-	def test_difference_method(self, method, env):
-		file_path = "/opt/checkpoints/decision_table.csv"
-		dt = DecisionTable({"method": method})
-		y_true = np.array([1] * 50000 + [0] * 50000)
-		np.random.shuffle(y_true)
-		y_pred = np.random.normal(0, 1, size=100000)
-		if method not in ["equal_frequency", "equal_width"]:
+	def test_methods(self, method, env):
+		io_file_path = "/opt/checkpoints/decision_table_{}.csv".format(method)
+		config = {"method": method}
+		if method not in ("equal_frequency", "equal_width"):
 			with pytest.raises(NotImplementedError) as e:
-				dt.fit(y_true, y_pred)
+				dt = DecisionTable(config)
 		else:
+			dt = DecisionTable(config)
+
+			# prepare a random test data
+			y_true = np.array([1] * 50000 + [0] * 50000)
+			y_pred = np.random.normal(0, 1, size=100000)
+			np.random.shuffle(y_true)
+
 			dt.fit(y_true, y_pred)
-			dt.save(file_path)
+			dt.save(io_file_path)
 			# read_from_file
-			df = pd.read_csv(file_path)
+			df = pd.read_csv(io_file_path)
 			if method == "equal_frequency":
 				assert (df["样本数"] == 100000 / dt.bin_number).all()
+			elif method == "equal_width":
+				max_value, min_value = y_pred.max(), y_pred.min()
+				interval = (max_value - min_value) / dt.bin_number
+				left = float(df["区间"].iloc[0].strip("(]").split(', ')[0])
+				right = float(df["区间"].iloc[0].strip("(]").split(', ')[1])
+				assert left <= min_value
+				np.testing.assert_almost_equal(right, min_value + interval, decimal=2)
+			else:
+				raise NotImplementedError("test failed.")
 
-	def test_threshold_cutter(self):
-		y = [1] * 10 + [0] * 40 + [1] * 40 + [0] * 10
-		p = np.arange(0.5, 1, 0.005)
+	def test_threshold_cutter_by_value(self):
+		io_file_path = "/opt/checkpoints/ks_plot.csv"
+
+		y = [1] * 100 + [0] * 400 + [1] * 400 + [0] * 100
+		p = np.arange(0.5, 1, 0.0005)
+
+		tc = ThresholdCutter(io_file_path)
+		tc.cut_by_value(y, p)
+		np.testing.assert_almost_equal(tc.bst_score, 0.6, decimal=3)
+		np.testing.assert_almost_equal(tc.bst_threshold, 0.75, decimal=3)
+		tc.save()
+
+		df = pd.read_csv(io_file_path)
+		assert (df["ks"] >= 0).all()
+		assert (df["ks"] <= 1.0).all()
+
+	def test_threshold_cutter_by_index(self):
+		y = [1] * 100 + [0] * 400 + [1] * 400 + [0] * 100
+		p = np.arange(0.5, 1, 0.0005)
+
 		tc = ThresholdCutter()
 		tc.cut_by_index(y, p)
-		np.testing.assert_almost_equal(tc.bst_score, 0.6)
-		np.testing.assert_almost_equal(tc.bst_threshold, 0.75)
-		tc2 = ThresholdCutter()
-		tc2.cut_by_value(y, p)
-		np.testing.assert_almost_equal(tc2.bst_score, 0.6)
-		np.testing.assert_almost_equal(tc2.bst_threshold, 0.7475)
+		np.testing.assert_almost_equal(tc.bst_score, 0.6, decimal=3)
+		np.testing.assert_almost_equal(tc.bst_threshold, 0.75, decimal=3)
 