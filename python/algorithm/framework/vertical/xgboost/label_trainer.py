fix(xgboost): fix incorrect result for categorial feataure in some cases (#20)
* fix(xgboost): fix incorrect result for categorial feataure in some cases

Co-authored-by: Hong.Wang <hong.wang@basebit.ai>

python/algorithm/framework/vertical/xgboost/label_trainer.py
@@ -43,7 +43,7 @@ class VerticalXgboostLabelTrainer(VerticalXgboostBase):
         super().__init__(train_conf, is_label_trainer=True, *args, **kwargs)
         self.party_id = FedConfig.node_id
 
-        self.channels = {}
+        self.channels = dict()
         self.channels["encryption_context"] = BroadcastChannel(
             name="encryption_context")
         self.channels["individual_grad_hess"] = BroadcastChannel(
@@ -110,7 +110,7 @@ class VerticalXgboostLabelTrainer(VerticalXgboostBase):
         self.export_conf = [{
             "class_name": "VerticalXGBooster",
             "identity": self.identity,
-            "filename": self.output.get("model", {"name": "vertical_xgboost_guest.pt"})["name"]
+            "filename": self.output.get("model", {"name": "vertical_xgboost_guest.json"})["name"]
         }]
 
     def fit(self):
@@ -468,7 +468,21 @@ class VerticalXgboostLabelTrainer(VerticalXgboostBase):
         boosting_tree = BoostingTree.from_dict(json_dict)
         return boosting_tree
 
+    def check_dataset(self):
+        self.channels["check_dataset_com"] = BroadcastChannel(name="check_dataset_com")
+        data_lens = self.channels["check_dataset_com"].collect()
+        n = data_lens[0]
+        for _ in data_lens:
+            if n != _:
+                raise ValueError("Lengths of the datasets mismatched.")
+        if self.test_dataset:
+            assert len(self.test_dataset), "Lengths of the datasets mismatched."
+        else:
+            self.test_dataset = NdarrayIterator(np.zeros((n, 0)), self.bs)
+            self.test_ids = np.arange(n)
+
     def predict(self):
+        self.check_dataset()
         boosting_tree = self.load_model()
         test_y_pred_primitive = self.predict_on_boosting_tree(boosting_tree=boosting_tree,
                                                               data_iterator=self.test_dataset)

python/algorithm/framework/vertical/xgboost/trainer.py
@@ -1,11 +1,11 @@
 # Copyright 2022 The XFL Authors. All rights reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,10 +37,11 @@ from .decision_tree_trainer import VerticalDecisionTreeTrainer
 class VerticalXgboostTrainer(VerticalXgboostBase):
     def __init__(self, train_conf: dict, *args, **kwargs):
         super().__init__(train_conf, is_label_trainer=False, *args, **kwargs)
-
-        self.channels = {}
-        self.channels["encryption_context"] = BroadcastChannel(name="encryption_context")
-        self.channels["individual_grad_hess"] = BroadcastChannel(name="individual_grad_hess")
+        self.channels = dict()
+        self.channels["encryption_context"] = BroadcastChannel(
+            name="encryption_context")
+        self.channels["individual_grad_hess"] = BroadcastChannel(
+            name="individual_grad_hess")
         self.channels["tree_node"] = BroadcastChannel(name="tree_node")
 
         self.channels["summed_grad_hess"] = DualChannel(name="summed_grad_hess_" + FedConfig.node_id,
@@ -59,34 +60,38 @@ class VerticalXgboostTrainer(VerticalXgboostBase):
         if isinstance(self.xgb_config.encryption_param, (PlainParam, type(None))):
             self.public_context = None
         elif isinstance(self.xgb_config.encryption_param, PaillierParam):
-            self.public_context = self.channels["encryption_context"].recv(use_pickle=False)
+            self.public_context = self.channels["encryption_context"].recv(
+                use_pickle=False)
             self.public_context = Paillier.context_from(self.public_context)
         else:
-            raise TypeError(f"Encryption param type {type(self.xgb_config.encryption_param)} not valid.")
+            raise TypeError(
+                f"Encryption param type {type(self.xgb_config.encryption_param)} not valid.")
         self.export_conf = [{
             "class_name": "VerticalXGBooster",
             "identity": self.identity,
-            "filename": self.output.get("model", {"name": "vertical_xgboost_host.pt"})["name"]
+            "filename": self.output.get("model", {"name": "vertical_xgboost_host.json"})["name"]
         }]
-        
-        ray.init(num_cpus=get_core_num(self.xgb_config.max_num_cores), 
+
+        ray.init(num_cpus=get_core_num(self.xgb_config.max_num_cores),
                  ignore_reinit_error=True)
 
     def fit(self):
         # nodes_dict = {}
         node_dict = NodeDict()
-        
+
         for tree_idx in range(self.xgb_config.num_trees):
             # training section
             logger.info("Tree {} start training.".format(tree_idx))
-            
+
             restart_status = 0
             while True:
                 sampled_features, feature_id_mapping = self.col_sample()
                 # col index in feature
-                cat_columns_after_sampling = list(filter(lambda x: feature_id_mapping[x] in self.cat_columns, list(feature_id_mapping.keys())))
-                split_points_after_sampling = [self.split_points[feature_id_mapping[k]] for k in feature_id_mapping.keys()]
-                
+                cat_columns_after_sampling = list(
+                    filter(lambda x: feature_id_mapping[x] in self.cat_columns, list(feature_id_mapping.keys())))
+                split_points_after_sampling = [self.split_points[feature_id_mapping[k]] for k in
+                                               feature_id_mapping.keys()]
+
                 trainer = VerticalDecisionTreeTrainer(tree_param=self.xgb_config,
                                                       features=sampled_features,
                                                       cat_columns=cat_columns_after_sampling,
@@ -96,40 +101,43 @@ class VerticalXgboostTrainer(VerticalXgboostBase):
                                                       feature_id_mapping=feature_id_mapping,
                                                       tree_index=tree_idx)
                 nodes = trainer.fit()
-                
+
                 restart_status = self.channels["restart_com"].recv()
-                
+
                 if restart_status != 1:
                     break
-                
+
                 logger.info(f"trainer tree {tree_idx} training restart.")
-                
+
             logger.info("Tree {} training done.".format(tree_idx))
             if restart_status == 2:
-                logger.info("trainer early stopped. because a tree's root is leaf.")
+                logger.info(
+                    "trainer early stopped. because a tree's root is leaf.")
                 break
-            
+
             node_dict.update(nodes)
-            
+
             if self.xgb_config.run_goss:
                 self.predict_on_tree(nodes, self.train_dataset)
 
             # valid section
-            logger.info("trainer: Validation on tree {} start.".format(tree_idx))
+            logger.info(
+                "trainer: Validation on tree {} start.".format(tree_idx))
             self.predict_on_tree(nodes, self.val_dataset)
-            
+
             if self.channels["early_stop_com"].recv():
                 logger.info("trainer early stopped.")
                 break
             logger.info("Validation on tree {} done.".format(tree_idx))
 
-            if self.interaction_params.get("save_frequency") > 0 and (tree_idx + 1) % self.interaction_params.get("save_frequency") == 0:
+            if self.interaction_params.get("save_frequency") > 0 and (tree_idx + 1) % self.interaction_params.get(
+                    "save_frequency") == 0:
                 self.save(node_dict, epoch=tree_idx + 1)
-                
+
         # model preserve
         self.save(node_dict, final=True)
         ray.shutdown()
-        
+
     def _make_indicator_for_prediction(self, nodes: Dict[str, Node], feature: np.ndarray):
         indicator = {}
         for node_id, node in nodes.items():
@@ -140,26 +148,28 @@ class VerticalXgboostTrainer(VerticalXgboostBase):
             else:
                 indicator[node_id] = (data < node.split_info.split_point)
         return indicator
-    
+
     def predict_on_tree(self, nodes: Dict[str, Node], data_iterator: NdarrayIterator):
         for data in data_iterator:
             indicator = self._make_indicator_for_prediction(nodes, data)
             indicator = {k: np.packbits(v) for k, v in indicator.items()}
             self.channels["val_com"].send(indicator)
-    
+
     def predict_on_boosting_tree(self, nodes: Dict[str, Node], data_iterator: NdarrayIterator):
         self.predict_on_tree(nodes, data_iterator)
-            
-    def save(self, node_dict: NodeDict,  epoch: int = None, final: bool = False):
+
+    def save(self, node_dict: NodeDict, epoch: int = None, final: bool = False):
         if final:
-            save_model_config(stage_model_config=self.export_conf, save_path=Path(self.output.get("model")["path"]))
+            save_model_config(stage_model_config=self.export_conf, save_path=Path(
+                self.output.get("model")["path"]))
 
         save_dir = str(Path(self.output.get("model")["path"]))
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
         model_name_list = self.output.get("model")["name"].split(".")
-        name_prefix, name_postfix = ".".join(model_name_list[:-1]), model_name_list[-1]
+        name_prefix, name_postfix = ".".join(
+            model_name_list[:-1]), model_name_list[-1]
         if not final and epoch:
             model_name = name_prefix + "_{}".format(epoch) + "." + name_postfix
         else:
@@ -184,8 +194,14 @@ class VerticalXgboostTrainer(VerticalXgboostBase):
         node_dict = NodeDict.from_dict(json_dict)
         return node_dict
 
+    def check_dataset(self):
+        self.channels["check_dataset_com"] = BroadcastChannel(
+            name="check_dataset_com")
+        n = len(self.test_dataset)
+        self.channels["check_dataset_com"].send(n)
+
     def predict(self):
+        self.check_dataset()
         node_dict = self.load_model()
         self.predict_on_boosting_tree(nodes=node_dict.nodes,
                                       data_iterator=self.test_dataset)
-        